dd7bd7f767d87fedccb5683232e36661f58ad698
Oluwayimika Ojo-Bello <yimzika@gmail.com>
2021-04-16 15:29:21 +0000

    removing encryption and decryption

--- routes/passwordRoutes/passwordRoutes.js
+++ routes/passwordRoutes/passwordRoutes.js
@@ -4,17 +4,13 @@ const router = express.Router()
 import Password from '../../models/dbPasswords.js'
 import User from '../../models/dbUser.js'
 import {isAuth} from '../../util.js'
-import {encrypt, decrypt} from "../../EncryptionHandler.js"
 
 router.get("/:id/",isAuth, async (req,res)=>{
     const owner = await User.findOne({_id:req.params.id});
     if(owner){
         const passwords = await Password.find({owner:owner.username})
-        var decryptedPassword = passwords;
-        for(i=0; i<passwords.length;i++){
-            decryptedPassword[i].password=decrypt(passwords);
-        }
-        res.send(decryptedPassword);
+
+        res.send(passwords);
     }
     else{
         res.status(404).send("No such user using this platform")
@@ -22,11 +18,10 @@ router.get("/:id/",isAuth, async (req,res)=>{
 });
 
 router.post("/",async(req,res)=>{
-    const hashedPassword =encrypt(req.body.password)
     const newPassword = new Password({
         title: req.body.title,
         owner: req.body.activeUser.username,
-        password: hashedPassword
+        password: req.body.password
 
     });
     const newPasswordCreated = await newPassword.save(function(err, password){
@@ -41,10 +36,9 @@ router.post("/",async(req,res)=>{
 router.put("/:id", async (req,res)=>{
     const passwordId = req.params.id;
     const password = await Password.findOne({_id:passwordId})
-    const hashedPassword= encrypt(req.body.password)
     if(password){
         password.title=req.body.title;
-        password.password= hashedPassword;
+        password.password= req.body.password;
         const updatedPassword = await password.save();
         if (updatedPassword){
             return res.status(200).send({
