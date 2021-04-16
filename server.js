8afed26250d920bd4a7fa9f13bba0d6635f942d4
Oluwayimika Ojo-Bello <yimzika@gmail.com>
2021-04-16 12:56:00 +0000

    added cors

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ import authRoutes from './routes/authRoutes/authRoutes.js'
 import passwordRoutes from './routes/passwordRoutes/passwordRoutes.js'
 //dbSchema for user
 import User from "./models/dbUser.js"
+import Cors from 'cors'
 
 //app config 
 const app = express();
 app.use(express.json())
-
+app.use(Cors());
 const port = process.env.PORT || 8001;
 // const connection_url= `mongodb+srv://Yimika:Nigeria2021@projects.i8x1w.mongodb.net/EcommerceProject?retryWrites=true&w=majority`
 const connection_url=`mongodb://Yimika:Nigeria2021@projects-shard-00-00.i8x1w.mongodb.net:27017,projects-shard-00-01.i8x1w.mongodb.net:27017,projects-shard-00-02.i8x1w.mongodb.net:27017/EcommerceProject?ssl=true&replicaSet=atlas-lueebu-shard-0&authSource=admin&retryWrites=true&w=majority`
